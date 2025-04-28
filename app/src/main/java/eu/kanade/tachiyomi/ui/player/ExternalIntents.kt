Noirero/Lalapo

feat(player): Implement video hosters (#1892)
Co-authored-by: Joël Miramon <joel@miramon.de>

app/src/main/java/eu/kanade/tachiyomi/ui/player/ExternalIntents.kt
@@ -22,6 +22,7 @@ import eu.kanade.tachiyomi.data.download.anime.AnimeDownloadManager
 import eu.kanade.tachiyomi.data.track.AnimeTracker
 import eu.kanade.tachiyomi.data.track.TrackerManager
 import eu.kanade.tachiyomi.ui.player.loader.EpisodeLoader
+import eu.kanade.tachiyomi.ui.player.loader.HosterLoader
 import eu.kanade.tachiyomi.ui.player.settings.PlayerPreferences
 import eu.kanade.tachiyomi.util.system.LocaleHelper
 import eu.kanade.tachiyomi.util.system.isOnline
@@ -80,12 +81,13 @@ class ExternalIntents {
         chosenVideo: Video?,
     ): Intent? {
         if (!initAnime(animeId, episodeId)) return null
+        val hosters = EpisodeLoader.getHosters(episode, anime, source)
 
         val video = chosenVideo
-            ?: EpisodeLoader.getLinks(episode, anime, source).firstOrNull()
+            ?: HosterLoader.getBestVideo(source, hosters)
             ?: throw Exception("Video list is empty")
 
-        val videoUrl = getVideoUrl(context, video) ?: return null
+        val videoUrl = getVideoUrl(source, context, video) ?: return null
 
         val pkgName = playerPreferences.externalPlayerPreference().get()
 
@@ -117,12 +119,14 @@ class ExternalIntents {
      * @param context the application context.
      * @param video the video being sent to the external player.
      */
-    private suspend fun getVideoUrl(context: Context, video: Video): Uri? {
-        if (video.videoUrl == null) {
-            makeErrorToast(context, Exception("Video URL is null."))
+    private suspend fun getVideoUrl(source: AnimeSource, context: Context, video: Video): Uri? {
+        val resolvedVideo = HosterLoader.getResolvedVideo(source, video)
+
+        if (resolvedVideo == null || resolvedVideo.videoUrl.isEmpty()) {
+            makeErrorToast(context, Exception("Video URL is empty."))
             return null
         } else {
-            val uri = video.videoUrl!!.toUri()
+            val uri = resolvedVideo.videoUrl.toUri()
 
             val isOnDevice = if (anime.source == LocalAnimeSource.ID) {
                 true

app/src/main/java/eu/kanade/tachiyomi/ui/player/loader/EpisodeLoader.kt
@@ -2,9 +2,13 @@ package eu.kanade.tachiyomi.ui.player.loader
 
 import eu.kanade.domain.items.episode.model.toSEpisode
 import eu.kanade.tachiyomi.animesource.AnimeSource
+import eu.kanade.tachiyomi.animesource.model.Hoster
+import eu.kanade.tachiyomi.animesource.model.Hoster.Companion.toHosterList
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
 import eu.kanade.tachiyomi.data.download.anime.AnimeDownloadManager
+import eu.kanade.tachiyomi.ui.player.controls.components.sheets.HosterState
+import kotlinx.coroutines.CancellationException
 import tachiyomi.domain.entries.anime.model.Anime
 import tachiyomi.domain.items.episode.model.Episode
 import tachiyomi.source.local.entries.anime.LocalAnimeSource
@@ -13,27 +17,24 @@ import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
 /**
- * Loader used to retrieve the video links for a given episode.
+ * Loader used to retrieve the hosters for a given episode.
  */
 class EpisodeLoader {
-
     companion object {
 
-        private var errorMessage = ""
-
         /**
-         * Returns an observable list of videos of an [episode] based on the type of [source] used.
+         * Returns a list of hosters of an [episode] based on the type of [source] used.
          *
          * @param episode the episode being parsed.
          * @param anime the anime of the episode.
          * @param source the source of the anime.
          */
-        suspend fun getLinks(episode: Episode, anime: Anime, source: AnimeSource): List<Video> {
+        suspend fun getHosters(episode: Episode, anime: Anime, source: AnimeSource): List<Hoster> {
             val isDownloaded = isDownload(episode, anime)
             return when {
-                isDownloaded -> isDownload(episode, anime, source)
-                source is AnimeHttpSource -> isHttp(episode, source)
-                source is LocalAnimeSource -> isLocal(episode)
+                isDownloaded -> getHostersOnDownloaded(episode, anime, source)
+                source is AnimeHttpSource -> getHostersOnHttp(episode, source)
+                source is LocalAnimeSource -> getHostersOnLocal(episode)
                 else -> error("source not supported")
             }
         }
@@ -56,57 +57,49 @@ class EpisodeLoader {
         }
 
         /**
-         * Returns an list of videos when the [episode] is online.
+         * Returns a list of hosters when the [episode] is online.
          *
          * @param episode the episode being parsed.
          * @param source the online source of the episode.
          */
-        private suspend fun isHttp(episode: Episode, source: AnimeHttpSource): List<Video> {
-            val videos = source.getVideoList(episode.toSEpisode())
-
-            videos.filter { it.videoUrl.isNullOrEmpty() }.forEach { video ->
-                video.status = Video.State.LOAD_VIDEO
-
-                try {
-                    video.videoUrl = source.getVideoUrl(video)
-                    video.status = Video.State.READY
-                } catch (e: Throwable) {
-                    video.status = Video.State.ERROR
-                }
+        private suspend fun getHostersOnHttp(episode: Episode, source: AnimeHttpSource): List<Hoster> {
+            // TODO(1.6): Remove else block when dropping support for ext lib <1.6
+            return if (source.javaClass.declaredMethods.any { it.name == "getHosterList" }) {
+                source.getHosterList(episode.toSEpisode())
+            } else {
+                source.getVideoList(episode.toSEpisode()).toHosterList()
             }
-
-            return videos
         }
 
         /**
-         * Returns an observable list of videos when the [episode] is downloaded.
+         * Returns the hoster when the [episode] is downloaded.
          *
          * @param episode the episode being parsed.
          * @param anime the anime of the episode.
          * @param source the source of the anime.
          */
-        private fun isDownload(
+        private fun getHostersOnDownloaded(
             episode: Episode,
             anime: Anime,
             source: AnimeSource,
-        ): List<Video> {
+        ): List<Hoster> {
             val downloadManager: AnimeDownloadManager = Injekt.get()
             return try {
                 val video = downloadManager.buildVideo(source, anime, episode)
-                listOf(video)
+                listOf(video).toHosterList()
             } catch (e: Throwable) {
                 emptyList()
             }
         }
 
         /**
-         * Returns an list of videos when the [episode] is from local source.
+         * Returns the hoster when the [episode] is from local source.
          *
          * @param episode the episode being parsed.
          */
-        private fun isLocal(
+        private fun getHostersOnLocal(
             episode: Episode,
-        ): List<Video> {
+        ): List<Hoster> {
             return try {
                 val (animeDirName, episodeName) = episode.url.split('/', limit = 2)
                 val fileSystem: LocalAnimeSourceFileSystem = Injekt.get()
@@ -118,13 +111,60 @@ class EpisodeLoader {
                 val video = Video(
                     videoUri.toString(),
                     "Local source: ${episode.url}",
-                    videoUri.toString(),
-                    videoUri,
                 )
-                listOf(video)
+                listOf(video).toHosterList()
             } catch (e: Exception) {
-                errorMessage = e.message ?: "Error getting links"
                 emptyList()
+            }
+        }
+
+        /**
+         * Returns a list of videos of a [hoster] based on the type of [source] used.
+         * Note that for every type of episode except non-downloaded online, `videoList`
+         * will be set to null.
+         *
+         * @param source the source of the anime.
+         * @param hoster the hoster.
+         */
+        private suspend fun getVideos(source: AnimeSource, hoster: Hoster): List<Video> {
+            return when {
+                hoster.videoList != null && source is AnimeHttpSource -> hoster.videoList!!.parseVideoUrls(source)
+                hoster.videoList != null -> hoster.videoList!!
+                source is AnimeHttpSource -> getVideosOnHttp(source, hoster)
+                else -> error("source not supported")
+            }
+        }
+
+        /**
+         * Returns a list of hosters when the [episode] is online.
+         *
+         * @param source the online source of the episode.
+         * @param hoster the hoster.
+         */
+        private suspend fun getVideosOnHttp(source: AnimeHttpSource, hoster: Hoster): List<Video> {
+            return source.getVideoList(hoster).parseVideoUrls(source)
+        }
+
+        // TODO(1.6): Remove after ext lib bump
+        private suspend fun List<Video>.parseVideoUrls(source: AnimeHttpSource): List<Video> {
+            return this.map { video ->
+                if (video.videoUrl != "null") return@map video
+
+                val newVideoUrl = source.getVideoUrl(video)
+                video.copy(videoUrl = newVideoUrl)
+            }
+        }
+
+        suspend fun loadHosterVideos(source: AnimeSource, hoster: Hoster): HosterState {
+            return try {
+                val videos = getVideos(source, hoster)
+                HosterState.Ready(hoster.hosterName, videos, List(videos.size) { Video.State.QUEUE })
+            } catch (e: Exception) {
+                if (e is CancellationException) {
+                    throw e
+                }
+
+                HosterState.Error(hoster.hosterName)
             }
         }
     }

xkana-shii/anikku

app/src/main/java/eu/kanade/tachiyomi/ui/player/ExternalIntents.kt
@@ -25,6 +25,7 @@ import eu.kanade.tachiyomi.source.Source
 import eu.kanade.tachiyomi.source.isNsfw
 import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.ui.player.loader.EpisodeLoader
+import eu.kanade.tachiyomi.ui.player.loader.HosterLoader
 import eu.kanade.tachiyomi.ui.player.settings.PlayerPreferences
 import eu.kanade.tachiyomi.util.system.LocaleHelper
 import eu.kanade.tachiyomi.util.system.isOnline
@@ -84,12 +85,13 @@ class ExternalIntents {
         chosenVideo: Video?,
     ): Intent? {
         if (!initAnime(animeId, episodeId)) return null
+        val hosters = EpisodeLoader.getHosters(episode, anime, source)
 
         val video = chosenVideo
-            ?: EpisodeLoader.getLinks(episode, anime, source).firstOrNull()
+            ?: HosterLoader.getBestVideo(source, hosters)
             ?: throw Exception("Video list is empty")
 
-        val videoUrl = getVideoUrl(context, video) ?: return null
+        val videoUrl = getVideoUrl(source, context, video) ?: return null
 
         val pkgName = playerPreferences.externalPlayerPreference().get()
 
@@ -139,12 +141,14 @@ class ExternalIntents {
      * @param context the application context.
      * @param video the video being sent to the external player.
      */
-    private suspend fun getVideoUrl(context: Context, video: Video): Uri? {
-        if (video.videoUrl == null) {
-            makeErrorToast(context, Exception("Video URL is null. Instead watch Suavemente!"))
+    private suspend fun getVideoUrl(source: Source, context: Context, video: Video): Uri? {
+        val resolvedVideo = HosterLoader.getResolvedVideo(source, video)
+
+        if (resolvedVideo == null || resolvedVideo.videoUrl.isEmpty()) {
+            makeErrorToast(context, Exception("Video URL is empty."))
             return null
         } else {
-            val uri = video.videoUrl!!.toUri()
+            val uri = resolvedVideo.videoUrl.toUri()
 
             val isOnDevice = if (anime.source == LocalSource.ID) {
                 true

app/src/main/java/eu/kanade/tachiyomi/ui/player/loader/EpisodeLoader.kt
@@ -1,10 +1,14 @@
 package eu.kanade.tachiyomi.ui.player.loader
 
 import eu.kanade.domain.episode.model.toSEpisode
+import eu.kanade.tachiyomi.animesource.AnimeSource
+import eu.kanade.tachiyomi.animesource.model.Hoster
+import eu.kanade.tachiyomi.animesource.model.Hoster.Companion.toHosterList
 import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
 import eu.kanade.tachiyomi.data.download.DownloadManager
-import eu.kanade.tachiyomi.source.Source
-import eu.kanade.tachiyomi.source.online.HttpSource
+import eu.kanade.tachiyomi.ui.player.controls.components.sheets.HosterState
+import kotlinx.coroutines.CancellationException
 import tachiyomi.domain.anime.model.Anime
 import tachiyomi.domain.episode.model.Episode
 import tachiyomi.source.local.LocalSource
@@ -13,27 +17,24 @@ import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
 /**
- * Loader used to retrieve the video links for a given episode.
+ * Loader used to retrieve the hosters for a given episode.
  */
 class EpisodeLoader {
-
     companion object {
 
-        private var errorMessage = ""
-
         /**
-         * Returns an observable list of videos of an [episode] based on the type of [source] used.
+         * Returns a list of hosters of an [episode] based on the type of [source] used.
          *
          * @param episode the episode being parsed.
          * @param anime the anime of the episode.
          * @param source the source of the anime.
          */
-        suspend fun getLinks(episode: Episode, anime: Anime, source: Source): List<Video> {
+        suspend fun getHosters(episode: Episode, anime: Anime, source: AnimeSource): List<Hoster> {
             val isDownloaded = isDownload(episode, anime)
             return when {
-                isDownloaded -> isDownload(episode, anime, source)
-                source is HttpSource -> isHttp(episode, source)
-                source is LocalSource -> isLocal(episode)
+                isDownloaded -> getHostersOnDownloaded(episode, anime, source)
+                source is AnimeHttpSource -> getHostersOnHttp(episode, source)
+                source is LocalSource -> getHostersOnLocal(episode)
                 else -> error("source not supported")
             }
         }
@@ -56,57 +57,49 @@ class EpisodeLoader {
         }
 
         /**
-         * Returns an list of videos when the [episode] is online.
+         * Returns a list of hosters when the [episode] is online.
          *
          * @param episode the episode being parsed.
          * @param source the online source of the episode.
          */
-        private suspend fun isHttp(episode: Episode, source: HttpSource): List<Video> {
-            val videos = source.getVideoList(episode.toSEpisode())
-
-            videos.filter { it.videoUrl.isNullOrEmpty() }.forEach { video ->
-                video.status = Video.State.LOAD_VIDEO
-
-                try {
-                    video.videoUrl = source.getVideoUrl(video)
-                    video.status = Video.State.READY
-                } catch (e: Throwable) {
-                    video.status = Video.State.ERROR
-                }
+        private suspend fun getHostersOnHttp(episode: Episode, source: AnimeHttpSource): List<Hoster> {
+            // TODO(1.6): Remove else block when dropping support for ext lib <1.6
+            return if (source.javaClass.declaredMethods.any { it.name == "getHosterList" }) {
+                source.getHosterList(episode.toSEpisode())
+            } else {
+                source.getVideoList(episode.toSEpisode()).toHosterList()
             }
-
-            return videos
         }
 
         /**
-         * Returns an observable list of videos when the [episode] is downloaded.
+         * Returns the hoster when the [episode] is downloaded.
          *
          * @param episode the episode being parsed.
          * @param anime the anime of the episode.
          * @param source the source of the anime.
          */
-        private fun isDownload(
+        private fun getHostersOnDownloaded(
             episode: Episode,
             anime: Anime,
-            source: Source,
-        ): List<Video> {
+            source: AnimeSource,
+        ): List<Hoster> {
             val downloadManager: DownloadManager = Injekt.get()
             return try {
                 val video = downloadManager.buildVideo(source, anime, episode)
-                listOf(video)
+                listOf(video).toHosterList()
             } catch (e: Throwable) {
                 emptyList()
             }
         }
 
         /**
-         * Returns an list of videos when the [episode] is from local source.
+         * Returns the hoster when the [episode] is from local source.
          *
          * @param episode the episode being parsed.
          */
-        private fun isLocal(
+        private fun getHostersOnLocal(
             episode: Episode,
-        ): List<Video> {
+        ): List<Hoster> {
             return try {
                 val (animeDirName, episodeName) = episode.url.split('/', limit = 2)
                 val fileSystem: LocalSourceFileSystem = Injekt.get()
@@ -118,13 +111,60 @@ class EpisodeLoader {
                 val video = Video(
                     videoUri.toString(),
                     "Local source: ${episode.url}",
-                    videoUri.toString(),
-                    videoUri,
                 )
-                listOf(video)
+                listOf(video).toHosterList()
             } catch (e: Exception) {
-                errorMessage = e.message ?: "Error getting links"
                 emptyList()
+            }
+        }
+
+        /**
+         * Returns a list of videos of a [hoster] based on the type of [source] used.
+         * Note that for every type of episode except non-downloaded online, `videoList`
+         * will be set to null.
+         *
+         * @param source the source of the anime.
+         * @param hoster the hoster.
+         */
+        private suspend fun getVideos(source: AnimeSource, hoster: Hoster): List<Video> {
+            return when {
+                hoster.videoList != null && source is AnimeHttpSource -> hoster.videoList!!.parseVideoUrls(source)
+                hoster.videoList != null -> hoster.videoList!!
+                source is AnimeHttpSource -> getVideosOnHttp(source, hoster)
+                else -> error("source not supported")
+            }
+        }
+
+        /**
+         * Returns a list of hosters when the [episode] is online.
+         *
+         * @param source the online source of the episode.
+         * @param hoster the hoster.
+         */
+        private suspend fun getVideosOnHttp(source: AnimeHttpSource, hoster: Hoster): List<Video> {
+            return source.getVideoList(hoster).parseVideoUrls(source)
+        }
+
+        // TODO(1.6): Remove after ext lib bump
+        private suspend fun List<Video>.parseVideoUrls(source: AnimeHttpSource): List<Video> {
+            return this.map { video ->
+                if (video.videoUrl != "null") return@map video
+
+                val newVideoUrl = source.getVideoUrl(video)
+                video.copy(videoUrl = newVideoUrl)
+            }
+        }
+
+        suspend fun loadHosterVideos(source: AnimeSource, hoster: Hoster): HosterState {
+            return try {
+                val videos = getVideos(source, hoster)
+                HosterState.Ready(hoster.hosterName, videos, List(videos.size) { Video.State.QUEUE })
+            } catch (e: Exception) {
+                if (e is CancellationException) {
+                    throw e
+                }
+
+                HosterState.Error(hoster.hosterName)
             }
         }
     }