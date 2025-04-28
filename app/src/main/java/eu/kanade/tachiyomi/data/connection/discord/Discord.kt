aniyomiorg/aniyomi

Feat: implement Discord account management enhancements and RPC restart functionality (#245)
* fix: correct indentation in CastManager for better readability

* feat: implement Discord account management enhancements and RPC restart functionality

app/src/main/java/eu/kanade/tachiyomi/data/connections/discord/Discord.kt
@@ -8,6 +8,12 @@ import android.graphics.Color
 import androidx.annotation.StringRes
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.connections.ConnectionsService
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import logcat.LogPriority
+import tachiyomi.core.common.util.system.logcat
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 
 class Discord(id: Long) : ConnectionsService(id) {
 
@@ -26,6 +32,75 @@ class Discord(id: Long) : ConnectionsService(id) {
 
     override suspend fun login(username: String, password: String) {
         // Not Needed
+    }
+
+    private val json = Injekt.get<Json>()
+
+    fun getAccounts(): List<DiscordAccount> {
+        val accountsJson = connectionsPreferences.discordAccounts().get()
+        return try {
+            if (accountsJson.isNotBlank()) {
+                json.decodeFromString<List<DiscordAccount>>(accountsJson)
+            } else {
+                emptyList()
+            }
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
+
+    fun addAccount(account: DiscordAccount) {
+        val accounts = getAccounts().toMutableList()
+        logcat(LogPriority.DEBUG) { "Debug: Adding account: $account" }
+
+        if (account.isActive) {
+            accounts.replaceAll { it.copy(isActive = false) }
+            connectionsPreferences.connectionsToken(this).set(account.token)
+        }
+
+        val index = accounts.indexOfFirst { it.id == account.id }
+        if (index >= 0) {
+            accounts[index] = account
+        } else {
+            accounts.add(account)
+        }
+
+        logcat(LogPriority.DEBUG) { "Debug: Updated accounts: $accounts" } // Debug log
+        saveAccounts(accounts)
+    }
+
+    fun removeAccount(accountId: String) {
+        val accounts = getAccounts().toMutableList()
+        accounts.removeAll { it.id == accountId }
+        saveAccounts(accounts)
+    }
+
+    fun setActiveAccount(accountId: String) {
+        val accounts = getAccounts().toMutableList()
+        accounts.replaceAll { it.copy(isActive = it.id == accountId) }
+        saveAccounts(accounts)
+        // Update active token and restart RPC
+        accounts.find { it.id == accountId }?.let { account ->
+            connectionsPreferences.connectionsToken(this).set(account.token)
+            // Trigger RPC restart
+            connectionsPreferences.enableDiscordRPC().set(false)
+            connectionsPreferences.enableDiscordRPC().set(true)
+        }
+    }
+
+    fun restartRichPresence() {
+        // Trigger RPC restart by toggling the preference
+        connectionsPreferences.enableDiscordRPC().set(false)
+        connectionsPreferences.enableDiscordRPC().set(true)
+    }
+
+    private fun saveAccounts(accounts: List<DiscordAccount>) {
+        try {
+            val accountsJson = json.encodeToString(accounts)
+            connectionsPreferences.discordAccounts().set(accountsJson)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
 // <-- AM (DISCORD)

app/src/main/java/eu/kanade/tachiyomi/ui/setting/connections/DiscordLoginActivity.kt
@@ -12,6 +12,7 @@ import android.webkit.WebViewClient
 import eu.kanade.domain.connections.service.ConnectionsPreferences
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.connections.ConnectionsManager
+import eu.kanade.tachiyomi.data.connections.discord.DiscordAccount
 import eu.kanade.tachiyomi.ui.base.activity.BaseActivity
 import eu.kanade.tachiyomi.util.system.toast
 import tachiyomi.i18n.MR
@@ -58,6 +59,41 @@ class DiscordLoginActivity : BaseActivity() {
     }
 
     private fun login(token: String) {
+        Thread {
+            try {
+                val response = okhttp3.OkHttpClient().newCall(
+                    okhttp3.Request.Builder()
+                        .url("https://discord.com/api/v10/users/@me")
+                        .addHeader("Authorization", token)
+                        .build(),
+                ).execute()
+
+                if (response.isSuccessful) {
+                    val body = response.body.string()
+                    val jsonObject = org.json.JSONObject(body!!)
+                    val id = jsonObject.getString("id")
+                    val username = jsonObject.getString("username")
+                    val avatarId = jsonObject.optString("avatar")
+                    val avatarUrl = if (avatarId.isNotEmpty()) {
+                        "https://cdn.discordapp.com/avatars/$id/$avatarId.png"
+                    } else {
+                        null
+                    }
+
+                    val account = DiscordAccount(
+                        id = id,
+                        username = username,
+                        avatarUrl = avatarUrl,
+                        token = token,
+                        isActive = true,
+                    )
+                    connectionsManager.discord.addAccount(account)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }.start()
+
         connectionsPreferences.connectionsToken(connectionsManager.discord).set(token)
         connectionsPreferences.setConnectionsCredentials(
             connectionsManager.discord,
@@ -66,6 +102,7 @@ class DiscordLoginActivity : BaseActivity() {
         )
         toast(MR.strings.login_success)
         applicationInfo.dataDir.let { File("$it/app_webview/").deleteRecursively() }
+        setResult(RESULT_OK)
         finish()
     }
 }

xkana-shii/anikku

app/src/main/java/eu/kanade/tachiyomi/data/connection/discord/Discord.kt
@@ -8,6 +8,13 @@ import android.graphics.Color
 import androidx.annotation.StringRes
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.connection.BaseConnection
+import eu.kanade.tachiyomi.data.connections.discord.DiscordAccount
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import logcat.LogPriority
+import tachiyomi.core.common.util.system.logcat
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 
 class Discord(id: Long) : BaseConnection(id) {
 
@@ -26,6 +33,75 @@ class Discord(id: Long) : BaseConnection(id) {
 
     override suspend fun login(username: String, password: String) {
         // Not Needed
+    }
+
+    private val json = Injekt.get<Json>()
+
+    fun getAccounts(): List<DiscordAccount> {
+        val accountsJson = connectionPreferences.discordAccounts().get()
+        return try {
+            if (accountsJson.isNotBlank()) {
+                json.decodeFromString<List<DiscordAccount>>(accountsJson)
+            } else {
+                emptyList()
+            }
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
+
+    fun addAccount(account: DiscordAccount) {
+        val accounts = getAccounts().toMutableList()
+        logcat(LogPriority.DEBUG) { "Debug: Adding account: $account" }
+
+        if (account.isActive) {
+            accounts.replaceAll { it.copy(isActive = false) }
+            connectionPreferences.connectionsToken(this).set(account.token)
+        }
+
+        val index = accounts.indexOfFirst { it.id == account.id }
+        if (index >= 0) {
+            accounts[index] = account
+        } else {
+            accounts.add(account)
+        }
+
+        logcat(LogPriority.DEBUG) { "Debug: Updated accounts: $accounts" } // Debug log
+        saveAccounts(accounts)
+    }
+
+    fun removeAccount(accountId: String) {
+        val accounts = getAccounts().toMutableList()
+        accounts.removeAll { it.id == accountId }
+        saveAccounts(accounts)
+    }
+
+    fun setActiveAccount(accountId: String) {
+        val accounts = getAccounts().toMutableList()
+        accounts.replaceAll { it.copy(isActive = it.id == accountId) }
+        saveAccounts(accounts)
+        // Update active token and restart RPC
+        accounts.find { it.id == accountId }?.let { account ->
+            connectionPreferences.connectionsToken(this).set(account.token)
+            // Trigger RPC restart
+            connectionPreferences.enableDiscordRPC().set(false)
+            connectionPreferences.enableDiscordRPC().set(true)
+        }
+    }
+
+    fun restartRichPresence() {
+        // Trigger RPC restart by toggling the preference
+        connectionPreferences.enableDiscordRPC().set(false)
+        connectionPreferences.enableDiscordRPC().set(true)
+    }
+
+    private fun saveAccounts(accounts: List<DiscordAccount>) {
+        try {
+            val accountsJson = json.encodeToString(accounts)
+            connectionPreferences.discordAccounts().set(accountsJson)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
 // <-- AM (DISCORD)

app/src/main/java/eu/kanade/tachiyomi/ui/setting/connection/DiscordLoginActivity.kt
@@ -12,6 +12,7 @@ import android.webkit.WebViewClient
 import eu.kanade.domain.connection.service.ConnectionPreferences
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.connection.ConnectionManager
+import eu.kanade.tachiyomi.data.connections.discord.DiscordAccount
 import eu.kanade.tachiyomi.ui.base.activity.BaseActivity
 import eu.kanade.tachiyomi.util.system.toast
 import tachiyomi.i18n.MR
@@ -58,6 +59,41 @@ class DiscordLoginActivity : BaseActivity() {
     }
 
     private fun login(token: String) {
+        Thread {
+            try {
+                val response = okhttp3.OkHttpClient().newCall(
+                    okhttp3.Request.Builder()
+                        .url("https://discord.com/api/v10/users/@me")
+                        .addHeader("Authorization", token)
+                        .build(),
+                ).execute()
+
+                if (response.isSuccessful) {
+                    val body = response.body.string()
+                    val jsonObject = org.json.JSONObject(body!!)
+                    val id = jsonObject.getString("id")
+                    val username = jsonObject.getString("username")
+                    val avatarId = jsonObject.optString("avatar")
+                    val avatarUrl = if (avatarId.isNotEmpty()) {
+                        "https://cdn.discordapp.com/avatars/$id/$avatarId.png"
+                    } else {
+                        null
+                    }
+
+                    val account = DiscordAccount(
+                        id = id,
+                        username = username,
+                        avatarUrl = avatarUrl,
+                        token = token,
+                        isActive = true,
+                    )
+                    connectionManager.discord.addAccount(account)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }.start()
+
         connectionPreferences.connectionsToken(connectionManager.discord).set(token)
         connectionPreferences.setConnectionsCredentials(
             connectionManager.discord,
@@ -66,6 +102,7 @@ class DiscordLoginActivity : BaseActivity() {
         )
         toast(MR.strings.login_success)
         applicationInfo.dataDir.let { File("$it/app_webview/").deleteRecursively() }
+        setResult(RESULT_OK)
         finish()
     }
 }